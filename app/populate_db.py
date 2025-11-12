9ea8fb731574635f3072bdaccb813429ddadcf35
davidampereira <up202405163@up.pt>
2025-11-12 15:14:38 +0000

    show_id agora aparece so com os numeros em vez de s+numero

--- app/populate_db.py
+++ app/populate_db.py
@@ -4,5 +4,6 @@ import db
 
 def first_populate(c):
   df = pd.read_csv("4-DisneyPlus/DisneyPlus.csv")
+  df["show_id"] = df["show_id"].str[1:].astype(int)
   df.to_sql("Shows", c, if_exists="replace", index=False)
   return c
