aba6b65ca27ac14faed859792ab09fc3317939bf
davidampereira <up202405163@up.pt>
2025-11-15 19:52:28 +0000

    Adicionei funcao para criar uma tabela nova para data em foi adicionado o show, tem de ser noutra tabela pq ha valores NULL, as outras funcoes para criar novas tabelas teem de ser muito alteradas

--- app/populate_db.py
+++ app/populate_db.py
@@ -1,8 +1,4 @@
 import pandas as pd
-import sqlite3
-
-from sqlalchemy import null
-import db
 
 def table_show_other(c, original_csv, other_table, column):
   df = pd.DataFrame(columns=["show_id", f"{column}"])
@@ -18,8 +14,7 @@ def table_show_other(c, original_csv, other_table, column):
     except Exception as e:
       continue # original_row n tem valor, ia dar erro
   df = pd.concat([df, pd.DataFrame(new_table_rows)], ignore_index=True)
-  print(df)
-  df.to_sql(f"{column}_show_id", c, if_exists="replace", index=False)
+  return df
 
 
 def custom_table_create(c, original_csv, column, table_name):
@@ -32,15 +27,27 @@ def custom_table_create(c, original_csv, column, table_name):
   df = df.rename(columns={column: "name"})
   df['id'] = range(1, len(df) + 1)
   df = df[["id", "name"]]
-  df.to_sql(table_name, c, if_exists="replace", index=False)
-  table_show_other(c, original_csv, df, column)
+  table_to_add = table_show_other(c, original_csv, df, column)
+  table_to_add.to_sql(table_name, c, if_exists="replace", index=False)
   original_csv = original_csv.drop(columns=[column])
   return original_csv
+
+def handle_date_added(c, original_csv):
+  df = original_csv[['show_id', 'date_added']].copy()
+  df[["month", "day", "year"]] = df['date_added'].str.split(expand=True)
+  df["day"] = df["day"].str.replace(",", "")
+  df = df.drop(columns=["date_added"])
+  df = df.dropna()
+  original_csv = original_csv.drop(columns=["date_added"])
+  df.to_sql("added_date", c, if_exists="replace", index=False)
+
 
 def first_populate(c):
   original_csv = pd.read_csv("4-DisneyPlus/DisneyPlus.csv")
   original_csv["show_id"] = original_csv["show_id"].str[1:].astype(int)
   original_csv = custom_table_create(c, original_csv, "cast", "actors")
-  original_csv = custom_table_create(c, original_csv, "listed_in", "genre")
+  original_csv = custom_table_create(c, original_csv, "listed_in", "genres")
+  original_csv = custom_table_create(c, original_csv, "rating", "ratings")
+  handle_date_added(c, original_csv)
   original_csv.to_sql("Shows", c, if_exists="replace", index=False)
   return c
