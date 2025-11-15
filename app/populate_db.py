0d04e8b66f73deed9661737acd6bb59a14e357ad
davidampereira <up202405163@up.pt>
2025-11-15 21:45:15 +0000

    Todas as pessoas (diretores e atores) numa lista, tenho de implementar para tambem os associar ao show, mas agr n

--- app/populate_db.py
+++ app/populate_db.py
@@ -9,8 +9,9 @@ def table_show_other(c, original_csv, other_table, column):
     try:
       for new_row in other_table.itertuples(index=False):
         attr_name = getattr(new_row, "name")
+        attr_id = getattr(new_row, "id")
         if attr_name in original_row:
-          new_table_rows.append({"show_id": f"{show_id}", f"{column}": attr_name})
+          new_table_rows.append({"show_id": f"{show_id}", f"{column}": attr_id})
     except Exception as e:
       continue # original_row n tem valor, ia dar erro
   df = pd.concat([df, pd.DataFrame(new_table_rows)], ignore_index=True)
@@ -28,7 +29,8 @@ def custom_table_create(c, original_csv, column, table_name):
   df['id'] = range(1, len(df) + 1)
   df = df[["id", "name"]]
   table_to_add = table_show_other(c, original_csv, df, column)
-  table_to_add.to_sql(table_name, c, if_exists="replace", index=False)
+  df.to_sql(table_name, c, if_exists="replace", index=False)
+  table_to_add.to_sql(f"{table_name}_show", c, if_exists="replace", index=False)
   original_csv = original_csv.drop(columns=[column])
   return original_csv
 
@@ -42,15 +44,24 @@ def handle_date_added(c, original_csv):
   df.to_sql("added_date", c, if_exists="replace", index=False)
   return original_csv
 
+def handle_people(c, original_csv):
+  df = pd.concat([original_csv['cast'], original_csv['director']], axis=0).reset_index(drop=True).to_frame('name')
+  df["name"] = df["name"].str.split(", ")
+  df = df.explode("name")
+  df = df.dropna(subset=["name"])
+  df["name"] = df["name"].str.strip()
+  df = df.drop_duplicates(subset=["name"])
+  df["id"] = range(1, len(df)+1)
+  df = df[["id", "name"]]
+  df.to_sql("people", c, if_exists="replace", index=False)
 
 def first_populate(c):
   original_csv = pd.read_csv("4-DisneyPlus/DisneyPlus.csv")
   original_csv["show_id"] = original_csv["show_id"].str[1:].astype(int)
-  original_csv = custom_table_create(c, original_csv, "cast", "actors")
   original_csv = custom_table_create(c, original_csv, "listed_in", "genres")
   original_csv = custom_table_create(c, original_csv, "rating", "ratings")
-  original_csv = custom_table_create(c, original_csv, "director", "directors")
   original_csv = custom_table_create(c, original_csv, "country", "countries")
   original_csv = handle_date_added(c, original_csv)
+  handle_people(c, original_csv)
   original_csv.to_sql("Shows", c, if_exists="replace", index=False)
   return c
